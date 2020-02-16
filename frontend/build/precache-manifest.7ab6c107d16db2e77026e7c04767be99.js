f0ff99585c6638745e7ac790434cacbd9c0a649c
Kevin Jiang <kevin51jiang@email.com>
2020-02-16 03:19:48 -0500

    build frontend

---
+++ frontend/build/precache-manifest.7ab6c107d16db2e77026e7c04767be99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b476f07924ed7a1602",
+    "url": "/static/css/main.20b870ca.chunk.css"
+  },
+  {
+    "revision": "b8b476f07924ed7a1602",
+    "url": "/static/js/main.b8b476f0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "eb1376cb51dc9e9bce48",
+    "url": "/static/js/2.eb1376cb.chunk.js"
+  },
+  {
+    "revision": "62c22e2e215180c8cbd3d2aa9246840c",
+    "url": "/index.html"
+  }
+];
